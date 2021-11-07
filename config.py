3e0f8d4d20cbce4276fbcf3ed342de993a86fd92
ArtemS66 <91483481+ArtemS66@users.noreply.github.com>
2021-11-07 16:54:49 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+TOKEN = '2119571614:AAFLdOgbd0JUzK8PCRL3M7h9QiDIiF7MCpE'
+
+keys = {
+    'биткоин' : 'BTC',
+    'эфириум' : 'ETH',
+    'солана' : 'SOL',
+    'XRP' : 'XRP',
+    'доллар' : 'USD',
+}
