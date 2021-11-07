0ffdb01d08c2b1770c1940dcf20699ce24256733
ArtemS66 <91483481+ArtemS66@users.noreply.github.com>
2021-11-07 17:02:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,7 @@
 TOKEN = '2119571614:AAFLdOgbd0JUzK8PCRL3M7h9QiDIiF7MCpE'
 
 keys = {
-    'биткоин' : 'BTC',
-    'эфириум' : 'ETH',
-    'солана' : 'SOL',
-    'XRP' : 'XRP',
+    'евро' : 'EUR',
+    'рубль' : 'RUB',
     'доллар' : 'USD',
 }
